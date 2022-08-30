b1698c1ae49c548d909ee1bba1cad4e21954686f
MatthiasBlc <mat.bouloc@gmail.com>
2022-08-30 11:54:19 +0200

    add db and faker

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_30_085848) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_30_095246) do
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "jwt_denylist", force: :cascade do |t|
     t.string "jti", null: false
     t.datetime "exp", null: false
